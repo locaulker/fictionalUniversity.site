9d7e74f2cf2ac50ba2b0d1855486503549521ac5
Lawrence Caulker <locaulker@gmail.com>
2018-04-17 12:46:32 -0400

    completed events post-type

--- page-past-events.php
+++ page-past-events.php
@@ -3,15 +3,34 @@
   <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">All Events</h1>
+      <h1 class="page-banner__title">Past Events</h1>
       <div class="page-banner__intro">
-        <p>See What is going on in our world.</p>
+        <p>A Recap of our Past Events.</p>
       </div>
     </div>  
   </div>
 
   <div class="container container--narrow page-section">
-    <?php while (have_posts()) : the_post(); ?>
+    <?php 
+      $today = date('Ymd');
+      $pastEvents = new WP_Query(array(
+        'paged' => get_query_var('paged', 1),
+        // 'posts_per_page' => 1,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '<',
+            'value' => $today,
+            'type' => 'numeric'
+          )
+        )
+      )); 
+    
+    while ($pastEvents->have_posts()) : $pastEvents->the_post(); ?>
       <div class="event-summary">
         <a class="event-summary__date t-center" href="#">
           <span class="event-summary__month">
@@ -32,10 +51,10 @@
           <p><?php echo wp_trim_words(get_the_content(), 18) ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
         </div>
       </div>
-    <?php endwhile; ?>
-    <?php echo paginate_links(); ?>
-    <hr class="section-break">
-    <div>Looking for a recap of Past Events? Checkout our <a class="btn btn--beige" href="<?php echo site_url('/past-events'); ?>">Past Events</a>Archive</div>
+    <?php endwhile; //wp_reset_postdata(); ?>
+    <?php echo paginate_links(array(
+      'total' => $pastEvents->max_num_pages
+    )); ?>
   </div><!-- .container -->
 
 <?php get_footer(); ?>
