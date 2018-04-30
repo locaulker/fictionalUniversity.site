ee2d1e5a9c4a108f5a9d2a51b4877ca8cb97e9a7
Lawrence Caulker <locaulker@gmail.com>
2018-04-30 11:12:32 -0400

    traditional wp search completed

---
+++ template-parts/content-post.php
@@ -0,0 +1,12 @@
+<div class="post-item">
+  <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+  <div class="metabox">
+    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> at <?php the_time('g:i a'); ?> in the <?php echo get_the_category_list(', '); ?> Category</p>
+  </div><!-- .metabox -->
+
+  <div class="generic-content">
+    <?php the_excerpt(); ?>
+    <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+  </div><!-- .generic-content -->
+</div><!-- .post-item -->
