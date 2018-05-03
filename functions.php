730d76f54a87bea973063af3f9596648aabbc759
Lawrence Caulker <locaulker@gmail.com>
2018-05-03 14:34:34 -0400

    completed section 19: user Generated Content

--- functions.php
+++ functions.php
@@ -7,6 +7,10 @@ function university_custom_rest() {
   register_rest_field('post', 'authorName', array(
     'get_callback' => function() {return get_the_author();}
   ));
+
+  register_rest_field('note', 'userNoteCount', array(
+    'get_callback' => function() {return count_user_posts(get_current_user_id(), 'note');}
+  ));
 }
 add_action( 'rest_api_init', 'university_custom_rest' );
 
@@ -152,4 +156,22 @@ function ourLoginCSS() {
 add_filter('login_headertitle', 'ourLoginTitle');
 function ourLoginTitle() {
   return get_bloginfo('name');
+}
+
+// Force note posts to be private
+add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
+function makeNotePrivate($data, $postarr) {
+  if ($data['post_type'] == 'note') {
+    if (count_user_posts(get_current_user_id(), 'note') > 4 AND !$postarr['ID']) {
+      die("You have reached your Note limit.");
+    }
+
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+  }
+
+  if ($data['post_type'] == 'note' AND $data['post_type'] != 'trash') {
+    $data['post_status'] = "private";
+  }
+  return $data;
 }
--- page-my-notes.php
+++ page-my-notes.php
@@ -13,6 +13,14 @@ get_header();
 
   <div class="container container--narrow page-section">
 
+    <div class="create-note">
+      <h2 class="headline headline--medium">Create New Note</h2>
+      <input class="new-note-title" placeholder="Title">
+      <textarea class="new-note-body" placeholder="Your note here..."></textarea>
+      <span class="submit-note">Create Note</span>
+      <span class="note-limit-message">Note limit Reached: Delete an existing note to make room for a new one.</span>
+    </div>
+
     <ul class="min-list link-list" id="my-notes">
       <?php
         $userNotes = new WP_Query(array(
@@ -24,17 +32,17 @@ get_header();
         while($userNotes->have_posts()) : $userNotes->the_post(); ?>
 
         <li data-id="<?php the_ID(); ?>">
-          <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+          <input readonly class="note-title-field" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>">
           <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
           <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
-          <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+          <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
           <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
         </li>
 
         <?php endwhile; ?>
     </ul>
 
-  </div>
+  </div><!-- .container -->
 
 <?php endwhile;
   get_footer(); ?>
